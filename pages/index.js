72155da6e4e58ee49bac52b2b3237631ac8d5b4b
https://github.com/leomaemoto/pool_analytics.git <leo.a1025@gmail.com>
2023-03-22 12:42:33 +0800

    ➕ added image

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { Footer } from "@/components/Footer";
+import { Footer } from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -40,6 +40,25 @@ export default function Home() {
           </div>
         </div>
 
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+          <div className={styles.thirteen}>
+            <Image
+              src="/thirteen.svg"
+              alt="13"
+              width={40}
+              height={31}
+              priority
+            />
+          </div>
+        </div>
         <Footer />
       </main>
     </>
